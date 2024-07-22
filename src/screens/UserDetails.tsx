01dce052e98b7560d044a9a43141633097644a38
Huy Trinh <htrinh@alpha-sense.com>
2024-07-22 13:12:18 +0300

    refactor: cleanup

---
+++ src/screens/UserDetails.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+export default function UserDetails() {
+    return (
+        <Container>User details</Container>
+    )
+}
---
+++ src/screens/UserList.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+export default function UserList() {
+    return (
+        <Container>User list</Container>
+    )
+}
